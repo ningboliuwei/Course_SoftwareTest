11de63550f16b8ef4f3852148c1b159a62f2d6ea
ningboliuwei <liuwei@nbu.edu.cn>
2013-11-07 21:33:51 +0800

    第一次提交

---
+++ ATM模拟/ATM模拟/Form1.Designer.cs
@@ -0,0 +1,423 @@
+﻿namespace ATM模拟
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.button13 = new System.Windows.Forms.Button();
+			this.button14 = new System.Windows.Forms.Button();
+			this.button15 = new System.Windows.Forms.Button();
+			this.button16 = new System.Windows.Forms.Button();
+			this.button17 = new System.Windows.Forms.Button();
+			this.button18 = new System.Windows.Forms.Button();
+			this.button19 = new System.Windows.Forms.Button();
+			this.button20 = new System.Windows.Forms.Button();
+			this.button21 = new System.Windows.Forms.Button();
+			this.button22 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Location = new System.Drawing.Point(13, 13);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(178, 210);
+			this.panel1.TabIndex = 0;
+			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(142, 124);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(31, 15);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "未设";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(142, 180);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(31, 15);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "取消";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(142, 68);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(31, 15);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "查询";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(142, 12);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(31, 15);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "取款";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 55);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(67, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "请输入密码";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(3, 111);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.PasswordChar = '*';
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(103, 22);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 229);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(40, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(59, 229);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(40, 40);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(105, 229);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(40, 40);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(150, 229);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(40, 40);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "取消";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(13, 275);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(40, 40);
+			this.button5.TabIndex = 4;
+			this.button5.Text = "4";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(59, 275);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(40, 40);
+			this.button6.TabIndex = 5;
+			this.button6.Text = "5";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(105, 275);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(40, 40);
+			this.button7.TabIndex = 6;
+			this.button7.Text = "6";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(151, 275);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(40, 40);
+			this.button8.TabIndex = 7;
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(12, 321);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(40, 40);
+			this.button9.TabIndex = 8;
+			this.button9.Text = "7";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(58, 321);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(40, 40);
+			this.button10.TabIndex = 9;
+			this.button10.Text = "8";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(105, 321);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(40, 40);
+			this.button11.TabIndex = 10;
+			this.button11.Text = "9";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.Location = new System.Drawing.Point(150, 321);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(40, 40);
+			this.button12.TabIndex = 11;
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// button13
+			// 
+			this.button13.Location = new System.Drawing.Point(12, 367);
+			this.button13.Name = "button13";
+			this.button13.Size = new System.Drawing.Size(40, 40);
+			this.button13.TabIndex = 12;
+			this.button13.Text = ".";
+			this.button13.UseVisualStyleBackColor = true;
+			this.button13.Click += new System.EventHandler(this.button13_Click);
+			// 
+			// button14
+			// 
+			this.button14.Location = new System.Drawing.Point(58, 367);
+			this.button14.Name = "button14";
+			this.button14.Size = new System.Drawing.Size(40, 40);
+			this.button14.TabIndex = 13;
+			this.button14.Text = "0";
+			this.button14.UseVisualStyleBackColor = true;
+			this.button14.Click += new System.EventHandler(this.button14_Click);
+			// 
+			// button15
+			// 
+			this.button15.Location = new System.Drawing.Point(104, 367);
+			this.button15.Name = "button15";
+			this.button15.Size = new System.Drawing.Size(40, 40);
+			this.button15.TabIndex = 14;
+			this.button15.Text = "00";
+			this.button15.UseVisualStyleBackColor = true;
+			this.button15.Click += new System.EventHandler(this.button15_Click);
+			// 
+			// button16
+			// 
+			this.button16.Location = new System.Drawing.Point(150, 367);
+			this.button16.Name = "button16";
+			this.button16.Size = new System.Drawing.Size(40, 40);
+			this.button16.TabIndex = 15;
+			this.button16.Text = "确定";
+			this.button16.UseVisualStyleBackColor = true;
+			this.button16.Click += new System.EventHandler(this.button16_Click);
+			// 
+			// button17
+			// 
+			this.button17.Location = new System.Drawing.Point(197, 13);
+			this.button17.Name = "button17";
+			this.button17.Size = new System.Drawing.Size(80, 40);
+			this.button17.TabIndex = 16;
+			this.button17.Text = "<";
+			this.button17.UseVisualStyleBackColor = true;
+			this.button17.Click += new System.EventHandler(this.button17_Click);
+			// 
+			// button18
+			// 
+			this.button18.Location = new System.Drawing.Point(197, 69);
+			this.button18.Name = "button18";
+			this.button18.Size = new System.Drawing.Size(80, 40);
+			this.button18.TabIndex = 17;
+			this.button18.Text = "<<";
+			this.button18.UseVisualStyleBackColor = true;
+			this.button18.Click += new System.EventHandler(this.button18_Click);
+			// 
+			// button19
+			// 
+			this.button19.Location = new System.Drawing.Point(197, 125);
+			this.button19.Name = "button19";
+			this.button19.Size = new System.Drawing.Size(80, 40);
+			this.button19.TabIndex = 18;
+			this.button19.Text = "<<<";
+			this.button19.UseVisualStyleBackColor = true;
+			this.button19.Click += new System.EventHandler(this.button19_Click);
+			// 
+			// button20
+			// 
+			this.button20.Location = new System.Drawing.Point(197, 181);
+			this.button20.Name = "button20";
+			this.button20.Size = new System.Drawing.Size(80, 40);
+			this.button20.TabIndex = 19;
+			this.button20.Text = "<<<<";
+			this.button20.UseVisualStyleBackColor = true;
+			this.button20.Click += new System.EventHandler(this.button20_Click);
+			// 
+			// button21
+			// 
+			this.button21.Location = new System.Drawing.Point(197, 229);
+			this.button21.Name = "button21";
+			this.button21.Size = new System.Drawing.Size(80, 86);
+			this.button21.TabIndex = 20;
+			this.button21.Text = "插卡(正确)";
+			this.button21.UseVisualStyleBackColor = true;
+			this.button21.Click += new System.EventHandler(this.button21_Click);
+			// 
+			// button22
+			// 
+			this.button22.Location = new System.Drawing.Point(197, 321);
+			this.button22.Name = "button22";
+			this.button22.Size = new System.Drawing.Size(80, 85);
+			this.button22.TabIndex = 21;
+			this.button22.Text = "插卡(错误)";
+			this.button22.UseVisualStyleBackColor = true;
+			this.button22.Click += new System.EventHandler(this.button22_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(290, 418);
+			this.Controls.Add(this.button22);
+			this.Controls.Add(this.button21);
+			this.Controls.Add(this.button20);
+			this.Controls.Add(this.button19);
+			this.Controls.Add(this.button18);
+			this.Controls.Add(this.button17);
+			this.Controls.Add(this.button16);
+			this.Controls.Add(this.button15);
+			this.Controls.Add(this.button14);
+			this.Controls.Add(this.button13);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ATM模拟器";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Button button13;
+		private System.Windows.Forms.Button button14;
+		private System.Windows.Forms.Button button15;
+		private System.Windows.Forms.Button button16;
+		private System.Windows.Forms.Button button17;
+		private System.Windows.Forms.Button button18;
+		private System.Windows.Forms.Button button19;
+		private System.Windows.Forms.Button button20;
+		private System.Windows.Forms.Button button21;
+		private System.Windows.Forms.Button button22;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label5;
+	}
+}
+
---
+++ ATM模拟/ATM模拟/Form1.cs
@@ -0,0 +1,458 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ATM模拟
+{
+	public partial class Form1 : Form
+	{
+		private int balance = 12000;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button21_Click(object sender, EventArgs e)
+		{
+			ShowPasswordScreen();
+			button21.Enabled = false;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			ShowInitialScreen();
+		}
+
+		private void ShowInitialScreen() //初始界面
+		{
+			ClearMenu();
+
+			label1.Visible = true; 
+			label1.Text = "欢迎使用ATM机";
+			textBox1.Visible = false;
+			label2.Visible = false;
+			label3.Visible = false;
+			label4.Visible = false;
+			label5.Visible = false;
+
+			button21.Enabled = true;
+			
+			
+		}
+
+		private void ShowPasswordScreen()
+		{
+			textBox1.Text = "";
+
+
+			label1.Text = "请输入密码";
+			label1.Visible = true;
+
+			textBox1.Visible = true;
+			textBox1.PasswordChar = '*';
+			textBox1.Focus();
+			ClearMenu();
+
+		}
+
+		private void ShowAmountScreen()
+		{
+			textBox1.Visible = true;
+			textBox1.PasswordChar = '\0';
+			ShowInfo("请输入金额\n（必须为100的倍数）");
+			textBox1.Focus();
+			textBox1.Clear();
+
+			label2.Visible = false;
+			label3.Visible = false;
+			label4.Visible = false;
+			label5.Visible = true;
+			label5.Text = "返回";
+
+			
+		}
+
+		private void ShowMainMenu()
+		{
+			ClearMenu();
+
+			textBox1.Visible = false;
+			label1.Visible = false;
+
+			label2.Visible = true;
+			label2.Text = "取款";
+
+			label3.Visible = true;
+			label3.Text = "查询";
+
+			label4.Visible = false;
+
+			label5.Visible = true;
+			label5.Text = "取消";
+
+			
+		}
+
+		private void ShowPasswordErrorScreen()
+		{
+			ShowInfo("密码错误，请重新输入");
+			textBox1.Text = "";
+			textBox1.Focus();
+
+			ClearMenu();
+		}
+
+		private void ShowInfo(string infoText)
+		{
+			label1.Visible = true;
+			label1.Text = infoText;
+		}
+
+		private void ShowWithDrawScreen()
+		{
+
+			ClearMenu();
+
+			textBox1.Visible = false;
+			label1.Visible = false;
+
+			label2.Visible = true;
+			label2.Text = "100";
+
+			label3.Visible = true;
+			label3.Text = "200";
+
+			label4.Visible = true;
+			label4.Text = "500";
+
+			label5.Visible = true;
+			label5.Text = "自定";
+		}
+
+		private void button16_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				int amount = 0;
+				//重新输入可以作为一个bug
+				if (label1.Text == "请输入密码" || label1.Text == "密码错误，请重新输入") //当前要求用户输入密码且密码为“123456”
+				{
+					if (textBox1.Text == "123456")
+					{
+						ShowMainMenu();
+					}
+					else
+					{
+						ShowPasswordErrorScreen();
+					}
+				}
+				else if (label1.Text.Contains("金额"))
+				{
+					if (textBox1.Text.Contains(".") == false)
+					{
+						amount = Convert.ToInt32(textBox1.Text.Trim());
+					}
+
+					//此处为bug 输入小数点后出错
+					if (textBox1.Text.Contains("."))
+					{
+						ShowInfo("系统崩溃");
+						textBox1.Visible = false;
+					}
+					else if (amount % 50 != 0) //输入的金额不是50的倍数（此处为bug，实际应为100倍数）
+					{
+
+						ShowInfo("输入的金额必须为100的倍数\n请重新输入");
+						textBox1.Text = "";
+						textBox1.Focus();
+					}
+					else if (amount > balance) 
+					{
+						ShowInfo("您的金额不足\n请重新输入");
+						textBox1.Text = "";
+						textBox1.Focus();
+					}
+					else
+					{
+						MessageBox.Show(string.Format("吐出{0}元钱", amount));
+						ShowMainMenu();
+						balance = balance - amount;
+					}
+				}
+			}
+		}
+
+		private void button18_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				if (label3.Text == "查询")
+				{
+					ShowMoney();
+				}
+				else if (label3.Text == "200") //此处2bug，一个是不作余额检查，另一个是选择200，实际余额只少了100
+				{
+					MessageBox.Show(string.Format("吐出{0}元钱", 200));
+					ShowMainMenu();
+					balance = balance - 100;
+				}
+			}
+		}
+
+		private void ShowMoney()
+		{
+			label3.Visible = true;
+			label3.Text = balance.ToString();
+
+			label5.Visible = true;
+			label5.Text = "返回";
+
+			ShowInfo("您的余额为");
+		}
+
+		private void button20_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				if (label5.Text == "取消")
+				{
+					MessageBox.Show("卡已退出");
+					ShowInitialScreen();
+				}
+				else if (label5.Text == "返回")
+				{
+					if (label3.Text == balance.ToString()) //之前是查询余额
+					{
+						ShowMainMenu(); //返回后显示主菜单
+					}
+					else if (label3.Text == "200") //之前是输入金额
+					{
+						ShowWithDrawScreen(); //返回后显示选择取款金额菜单
+					}
+				}
+				else if (label5.Text == "自定")
+				{
+					ShowAmountScreen();
+				}
+			}
+		}
+
+		private void button17_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				if (label2.Text == "取款")//此处bug，不作余额检查
+				{
+					ShowWithDrawScreen();
+				}
+				else if (label2.Text == "100")
+				{
+					MessageBox.Show(string.Format("吐出{0}元钱", 100));
+					ShowMainMenu();
+					balance = balance - 100;
+				}
+			}
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "1";
+			}
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "2";
+			}
+		}
+
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "3";
+			}
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "4";
+			}
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "5";
+			}
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "6";
+			}
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "7";
+			}
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "8";
+			}
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "9";
+			}
+		}
+
+		private void button13_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + ".";
+			}
+		}
+
+		private void button14_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "0";
+			}
+		}
+
+		private void button15_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				textBox1.Text = textBox1.Text + "00";
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				if (label1.Text.Contains("密码"))
+				{
+					textBox1.Clear();
+				}
+
+				if (label2.Text == "100") //在金额选择界面
+				{
+					ShowMainMenu();
+				}
+			}
+		}
+
+		private void button19_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+				if (500 > balance)
+				{
+					ShowInfo("您的金额不足\n请重新输入");
+					textBox1.Text = "";
+					textBox1.Focus();
+				}
+				else if (label4.Text == "500") //此处为bug 取500，只吐了400
+				{
+					MessageBox.Show(string.Format("吐出{0}元钱", 400));
+					ShowMainMenu();
+					balance = balance - 500;
+				}
+			}
+		}
+
+		private void button22_Click(object sender, EventArgs e)
+		{
+
+			button21.Enabled = false;
+			ShowInfo("插卡错误，请重新插卡");
+			System.Threading.Thread.Sleep(1000);
+			MessageBox.Show("卡已吐");
+			button21.Enabled = true;
+			ShowInitialScreen();
+
+		}
+
+		private void ClearMenu()
+		{
+			label2.Visible = false;
+			label2.Text = "";
+
+			label3.Visible = false;
+			label3.Text = "";
+
+			label4.Visible = false;
+			label4.Text = "";
+
+			label5.Visible = false;
+			label5.Text = "";
+
+		}
+
+		private bool CheckCard()
+		{
+			if (button21.Enabled == true)
+			{
+				ShowInfo("请先插卡");
+				//System.Threading.Thread.Sleep(1000);
+				MessageBox.Show("好吧，我知道了，插卡先");
+				ShowInitialScreen();
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+			}
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			if (CheckCard())
+			{
+			}
+		}
+	}
+}
